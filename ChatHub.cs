1916403975cea3ffb3c2cb941cc18db484e3a9c5
AtlasLigz <a96967965@gmail.com>
2022-01-14 21:17:51 +0800

    Chat

--- ChatHub.cs
+++ ChatHub.cs
@@ -8,6 +8,16 @@ namespace LMSweb
 {
     public class ChatHub : Hub
     {
+        public void Group(String GroupId)
+        {
+            Groups.Add(Context.ConnectionId, GroupId);
+            Clients.Group(GroupId).addMessage("Welcome");
+        }
+
+        public void GroupSend(string GroupId, string name, string Message)
+        {
+            Clients.Group(GroupId).groupMessage(name, DateTime.Now.ToString("HH:mm"), Message);
+        }
         public void Send(string name, string message)
         {
             // Call the addNewMessageToPage method to update clients.
