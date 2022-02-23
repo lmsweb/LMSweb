0e5213ddee2c5ef948ecb25bc4aeec94cbb65d0d
林世楷 <xy238562227@gmail.com>
2022-02-24 00:06:22 +0800

    editor的共同編輯demo

---
+++ CodeServiceHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMSweb
+{
+    public class CodeServiceHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
--- Scripts/Coding/coding_editor.js
+++ Scripts/Coding/coding_editor.js
@@ -1,15 +1,5 @@
-function storeData(userInput){
-    let data = {
-        sid : "S001",
-        mid : "M001",
-        actionType : userInput.action, 
-        lineNumber : userInput.line, 
-        content : userInput.content
-    }
-}
-
 //根據DOM元素的id構造出一個編輯器
-var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     mode: {
         name: "python",
         version: 3,
@@ -21,82 +11,11 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     keyMap: "vim",    //綁定Vim
 });
 
-let inputChList = new Array();
-let deleteChList = new Array();
-let inputString = "";
-let deleteString = "";
-editor.on('change', (ins, ch)=>{
-
-    let actionType = "";
-    let index = Number(ch.from.ch.toString());
-    let lineNumbers = ins.getCursor().line;
-    inputChList = Array.from(editor.getLine(lineNumbers).toString());
-
-    if(ch.origin == "+input")
-    {
-        // inputChList = Array.from(editor.getLine(lineNumbers).toString());
-        // console.log("inputChList.length " + inputChList.length + " index " + index);
-
-        actionType = "新增";
-        if(ch.text.length > 1 && inputChList.length <= index)    //字尾換行
-        {
-            console.log("動作：" + actionType + " 行號：" + lineNumbers + " 內容：" + inputString);
-
-            clearTmp();
-        }
-        else if(ch.text.length > 1 && inputChList.length > index)    //非字尾換行
-        {
-            let sub = inputString.substring(0, index);
-            let sub2 = inputString.substring(index);
-            console.log("動作：" + actionType + " 行號：" + lineNumbers + " 內容：" + sub);
-            console.log("動作：" + actionType + " 行號：" + (lineNumbers+1) + " 內容：" + sub2);
-            clearTmp();
-        }
-
-        // else if((ch.from.sticky != null) && (ch.from.sticky.toString().toLowerCase() == "before") && (ch.from.xRel != null))   //修改該行內容
-        // {
-        //     console.log("動作：" + "修改" + " 行號：" + (lineNumbers+1) + " 內容：" + inputString);
-        //     clearTmp();
-        // }
-
-        else    //輸入一個字元
-        {
-            if(ch.text[0] != "" && ch.text[0] != null)
-            {
-                // inputChList.splice(index, 0, ch.text[0]);
-            }
-            
-        }
-        
-    }
-    else if(ch.origin == "+delete")    
-    {
-        if((ch.from.sticky != null) && (ch.from.sticky.toString() == "1"))    //刪除一個字元
-        {
-            // inputChList.splice(index, 1);
-            deleteChList.push(ch.removed[0]);
-        }
-        else if((ch.from.sticky != null) && (ch.from.sticky.toString() == "before") && (ch.removed.length > 1))    //刪除一行
-        {
-            deleteString = deleteChList.reverse().join('');
-
-            console.log("動作：刪除 行號：" + (lineNumbers+2) + " 內容：" + deleteString);
-            clearTmp();
-        }
-        
-    }
-    
-    inputString = inputChList.join('');
-    deleteString = deleteChList.join('');
-    //console.log(inputString);
-
-    
-});
-
-function clearTmp()
-{
-    inputString = "";
-    inputChList.length = 0;
-    deleteString = "";
-    deleteChList.length = 0;
+let online = $.connection.codeServiceHub;
+online.client.hello = function () {
+    $('.CodeMirror').text( "print('Hello')");
 }
+
+$.connection.hub.start().done(function () {
+    online.server.hello();
+});
