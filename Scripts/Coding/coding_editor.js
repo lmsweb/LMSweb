9c6a46afdafe442a5d2521e4f1911eef1324975d
Chi772 <m10902013@ms.chu.edu.tw>
2022-03-05 03:40:02 +0800

    Server setting

--- Scripts/Coding/coding_editor.js
+++ Scripts/Coding/coding_editor.js
@@ -8,40 +8,46 @@ const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     lineNumbers: true,
     theme: "seti",
     fullScreen: true,    //全屏模式
-    //keyMap: "vim",    //綁定Vim
+    keyMap: "vim",    //綁定Vim
 });
 
 let online = $.connection.codeServiceHub;
 //online.client.hello = function () {
 //    editor.setValue("print(Hello);");
 //}
-console.log(document.getElementById("gid").value);
+
+$.connection.hub.start().done(function () {
+
+});
 
 
 let oldContent = "";
 let newContent = "";
-let cursor = { line: 0, ch: 0 };
-
-$.connection.hub.start().done(function () {
-    editor.setValue("");
-    editor.setCursor(cursor);
-});
+let cursor = { line : 0, ch : 0};
 
 editor.on('change', (ins, ch) => {
 
-    cursor.line = editor.getCursor().line;
-    cursor.ch = editor.getCursor().ch;
+    //console.log(editor.getCursor());
 
+    newContent = editor.getValue();
 
-    if (ch.origin == "+input" || ch.origin == "+delete") {
-        online.server.editCode(editor.getValue(), cursor.line, cursor.ch);
+    if (oldContent != newContent) {
+        cursor.line = editor.getCursor().line;
+        cursor.ch = editor.getCursor().ch;
+
+        online.server.editCode("name", editor.getValue());
+        oldContent = newContent;
+        
+        console.log(cursor);
+        editor.setCursor(cursor);
     }
+    
 
 });
 
 
-online.client.broadcastCode = function (content, line , ch) {
+online.client.broadcastCode = function (name, content) {
 
     editor.setValue(content);
-    editor.setCursor(line, ch);
+
 }
