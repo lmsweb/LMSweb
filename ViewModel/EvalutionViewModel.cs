1582553d79703effda21051b159edb5d4a0de577
AtlasLigz <a96967965@gmail.com>
2022-03-15 20:01:19 +0800

    組間互評 各組介面

--- ViewModel/EvalutionViewModel.cs
+++ ViewModel/EvalutionViewModel.cs
@@ -17,9 +17,14 @@ namespace LMSweb.ViewModel
         public string DrawingImgPath { get; set; }
         public string CodeText { get; set; }
         public IEnumerable<LMSweb.Models.Question> Questions { get; set; }
+        public IEnumerable<LMSweb.Models.DefaultQuestion> DefaultQuestions { get; set; }
         public List<Evalution_Response> ERs { get; set; }
+        public List<Group_Response> GRs { get; set; }
         public LMSweb.Models.Option Option { get; set; }
+        public LMSweb.Models.GroupER GroupER { get; set; }
         public LMSweb.Models.EvalutionResponse Response { get; set; }
+
+        public LMSweb.Models.DefaultOption DefaultOption { get; set; }
 
     }
     public class Evalution_Response
@@ -31,6 +36,18 @@ namespace LMSweb.ViewModel
         public string response { get; set; }
         public string comments { get; set; }
         public string sid { get; set; }
+
+    }
+
+    public class Group_Response
+    {
+        public int qid { get; set; }
+
+
+        [Required]
+        public string response { get; set; }
+        public string comments { get; set; }
+        public string gid { get; set; }
 
     }
 }
